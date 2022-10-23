ea7fc5aa886000dd5506cf3099a3b7218be6c216
Wingchi Tam <41390970+wingchi-tam@users.noreply.github.com>
2022-10-22 22:51:14 -0500

    questions completed

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
-@app.route('/')
-def home():
-    return render_template('index.html')
+
+# @app.route('/')
+# def home():
+#     return render_template('index.html')
+
+@app.route('/', methods =["GET", "POST"])
+def gfg():
+    if request.method == "POST":
+       # getting input with name = fname in HTML form
+       first_name = request.form.get("name")
+       return "Your name is "+first_name 
+    return render_template("index.html")
+
 if __name__ == '__main__':
     app.run(debug=True)
