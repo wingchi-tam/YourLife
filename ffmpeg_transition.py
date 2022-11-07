0198ff8c5f59e2c7c1fe5ea8ce60bcef64fc9f63
Wingchi Tam <41390970+wingchi-tam@users.noreply.github.com>
2022-11-07 15:20:32 -0600

    working ffmpeg transition

--- ffmpeg_transition.py
+++ ffmpeg_transition.py
@@ -5,22 +5,28 @@ from moviepy.editor import *
 
 ListImg = sorted(os.listdir('img'))
 
-# vidList = []
-for img in ListImg: 
-    output_path = os.path.join('vid', str(img)+'.mp4')
-    imgFile = os.path.join('img', img)
-    video = ffmpeg.input(imgFile)
-    effect_video = ffmpeg.zoompan(video, d = 100, z = 'zoom+0.001')
-    output = ffmpeg.output(effect_video, output_path).run()
+# # vidList = []
+# for img in ListImg: 
+#     output_path = os.path.join('vid', str(img)+'.mp4')
+#     imgFile = os.path.join('img', img)
+#     video = ffmpeg.input(imgFile)
+#     effect_video = ffmpeg.zoompan(video, d = 100, z = 'zoom+0.001')
+#     output = ffmpeg.output(effect_video, output_path).run()
 #concatinate all vid 
 
+ListVid = sorted(os.listdir('vid'))
+
+filenames = []
 clips = []
-for filename in os.listdir('vid'):
-    # print(filename)
+for filename in ListVid:
     if filename.endswith('.mp4'):
-        # order = int(filename[:6])
-        clip = VideoFileClip(os.path.join('vid', filename))
-        clips.append(clip)
+        filenames.append(filename)
+
+sorted(filenames)
+
+for filename in filenames: 
+    clip = VideoFileClip(os.path.join('vid', filename))
+    clips.append(clip)
 
 video = concatenate_videoclips(clips, method='compose')
 video.write_videofile('transition_test.mp4', fps=30)
